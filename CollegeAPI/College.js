51d8357961cdd1e6eea6486fe3ab4b7079a0bf74
LeoEASC6 <leo.everth101@gmail.com>
2019-08-14 10:38:36 -0400

    API working well

--- CollegeAPI/College.js
+++ CollegeAPI/College.js
@@ -1,5 +1,5 @@
 // FETCH DATA FROM URL
-const url ="https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,school.state,cost.net_price.public.by_income_level.0-30000&page=100&api_key=4nEXfWhJBDffgF9iL36vO9dgdY1iCAF0fQS2wa60"
+const url ="https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,school.state,2017.admissions.admission_rate.overall,2017.cost.avg_net_price.public,2017.cost.avg_net_price.private,school.school_url,2017.cost.tuition.in_state,2017.cost.tuition.out_of_state&page=100&api_key=4nEXfWhJBDffgF9iL36vO9dgdY1iCAF0fQS2wa60"
 //Y1iCAF0fQS2wa60"
 fetch(url).then(function(response){
    return response.json()
@@ -8,12 +8,12 @@ fetch(url).then(function(response){
    let data = myJson
    return data;
 })
-console.log(data);
-function filterColleges(obj){
-   const dataState = document.createElement("div")
-   dataState.className="state"
-   const dState = obj.school.state;
-   const state = school.state.filter(function(results: school.state));
-   dataState.appendChild(state);
-   console.log(dataState);
-};
+// console.log(data);
+// function filterColleges(obj){
+//    const dataState = document.createElement("div")
+//    dataState.className="state"
+//    const dState = obj.school.state;
+//    const state = school.state.filter(function(results: school.state));
+//    dataState.appendChild(state);
+//    console.log(dataState);
+// };
