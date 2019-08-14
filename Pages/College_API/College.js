3e336ee0ebc66b955e5719f208960854ee7d389b
michaelnASC6 <mnewby0503@gmail.com>
2019-08-14 15:11:01 -0400

    State Filter

--- Pages/College_API/College.js
+++ Pages/College_API/College.js
@@ -1,22 +1,25 @@
 // FETCH DATA FROM URL
+const url ="https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,school.state,2017.admissions.admission_rate.overall,2017.cost.avg_net_price.public,2017.cost.avg_net_price.private,school.school_url,2017.cost.tuition.in_state,2017.cost.tuition.out_of_state&page=100&api_key=4nEXfWhJBDffgF9iL36vO9dgdY1iCAF0fQS2wa60"
+let filterKey;
+let results;
 
-const url ="https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name,id,school.state,cost.net_price.public.by_income_level.0-30000&page=100&api_key=4nEXfWhJBDffgF9iL36vO9dgdY1iCAF0fQS2wa60"
+const button = document.querySelector("#filter-button");
 
-//Y1iCAF0fQS2wa60"
+function filterByState(arr, key){
+    const filteredArray = arr.filter(item => item["school.state"] === key)
+    console.log(filteredArray);
+}
 
-fetch(url).then(function(response){
-    return response.json()
-}).then(function(myJson) {
-   console.log(myJson);
-    let data = myJson
-    return data; 
-})
-console.log(data);
-function filterColleges(obj){
-    const dataState = document.createElement("div")
-    dataState.className="state"
-    const dState = obj.school.state;
-    const state = school.state.filter(function(results: school.state));
-    dataState.appendChild(state);    
-    console.log(dataState);
-};
+function getValue(){
+    const inputElement = document.querySelector("#stateForm").value;
+        filterKey = inputElement;
+filterByState(results, filterKey);}
+
+button.addEventListener("click", getValue);
+
+function getResults(obj){
+    results = obj.results;
+    console.log(results)
+}
+
+fetch (url).then(response => response.json()).then(myJson => getResults(myJson));
